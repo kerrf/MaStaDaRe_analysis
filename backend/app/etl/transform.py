57136cd47d4af2b62b147f691ac59b3956e4825f
ker_f <m.sokol98@web.de>
2026-08-14 01:17:40 +0200

    refactor: better naming

--- backend/app/etl/transform.py
+++ backend/app/etl/transform.py
@@ -1,27 +1,32 @@
 import logging
 from pathlib import Path
 
-from sqlalchemy import text
+from sqlalchemy import text, Engine
 from app.db.database import engine  # Your DB connection
 from app.services.logger import setup_logging
 
-logger = logging.getLogger(__name__)
-
 # TODO: Change query to choose only columns I truly need
-def run_sql_file(file_path: Path) -> None:
+def run_sql_file(engine: Engine, query_path: Path) -> None:
     """Reads and executes a pure SQL file."""
-    logger.info(f"Executing SQL transformation: {file_path.name}")
+    logger.info(f"Executing SQL transformation: {query_path.name}")
     
-    with open(file_path, 'r') as file:
-        sql_query = file.read()
+    with open(query_path, 'r') as query:
+        sql_query = text(query.read())
         
     with engine.begin() as conn:
-        conn.execute(text(sql_query))
+        conn.execute(sql_query)
         
     logger.info("Transformation complete.")
 
+
 if __name__ == "__main__":
+    
+    logger = logging.getLogger(__name__)
     setup_logging()
+    
     # This runs when your nightly cron job or task scheduler triggers the script
-    query_path = Path("app/etl/queries/build_staging_solar.sql")
-    run_sql_file(query_path)
+    # query_path = Path("app/etl/queries/aggregate_plz_einwohner.sql")
+    # run_sql_file(engine, query_path)
+    
+    query_path = Path("app/etl/queries/aggregate_by_bundesland.sql")
+    run_sql_file(engine, query_path)
